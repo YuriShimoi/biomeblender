50caccf117997a9c9973328be031a708b39225b5
Yuri Shimoi Rodrigues <yuri.shimoi@gatec.com.br>
2022-08-29 17:53:10 -0300

    Added initial layers for BlendedMap

--- test.js
+++ test.js
@@ -1,8 +1,26 @@
-function randomMap(variants=10) {
+function sampleMap() {
+    let _ = 0;
+    return [
+        [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_],
+        [_,_,_,1,1,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,3,3,_,_,_],
+        [_,_,1,_,_,_,_,_,6,6,6,6,6,6,6,6,6,_,_,_,_,_,3,_,_],
+        [_,1,_,_,_,_,_,6,_,_,_,_,_,_,_,_,_,6,_,_,_,_,_,3,_],
+        [_,1,_,_,_,_,6,_,_,_,_,_,_,_,_,_,_,_,6,_,_,_,_,3,_],
+        [_,_,_,_,_,6,_,_,_,_,_,5,5,5,_,_,_,_,_,6,_,_,_,_,_],
+        [_,_,_,_,_,6,_,_,_,_,_,5,5,5,_,_,_,_,_,6,_,_,_,_,_],
+        [_,2,_,_,_,_,6,_,_,_,_,_,_,_,_,_,_,_,6,_,_,_,_,4,_],
+        [_,2,_,_,_,_,_,6,_,_,_,_,_,_,_,_,_,6,_,_,_,_,_,4,_],
+        [_,_,2,_,_,_,_,_,6,6,6,6,6,6,6,6,6,_,_,_,_,_,4,_,_],
+        [_,_,_,2,2,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,4,4,_,_,_],
+        [_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_,_]
+    ];
+}
+
+function randomMap(variants=5) {
     let [sx, sy] = [Math.ceil(Math.random()*20)+3, Math.ceil(Math.random()*20)+3];
     return new Array(sx).fill(0).map(_ => new Array(sy).fill(0).map(_ => Math.ceil(Math.random()*variants))); 
 }
 
 let _document_ready = setInterval((f) => {if(document.readyState == "complete"){clearInterval(_document_ready);delete _document_ready;f()}}, 1, () => {
-    Visualizer.render(randomMap(5), document.getElementById('main'));
+    Visualizer.render(sampleMap(), document.getElementById('main'), ['lightgray'], true);
 });
